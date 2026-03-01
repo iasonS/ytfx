469aa3853b5cb923cfa58f8da40bc197f31af631
iasonS <sklavenitisi6@gmail.com>
2026-03-01 22:53:04 +0100

    Fix: use default import for youtube-dl-exec CommonJS module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { exec } from 'youtube-dl-exec';
+import youtubeDlExec from 'youtube-dl-exec';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
+const exec = youtubeDlExec;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
